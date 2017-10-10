bed06aee238126aafdbfdd554a82350ea3f28ab5
Anthony <huang.anthony.sy@gmail.com>
2017-10-10 08:27:17 -0700

    [What] Feature [Why] [How]

---
+++ src/components/ProductCmp.vue
@@ -0,0 +1,33 @@
+<style scoped>
+.ProductCmp{
+    padding: 20px;
+}
+img{
+    width: 100%;
+    display: block;
+}
+h2{
+    text-align: left;
+    margin: 0;
+}
+</style>
+
+<template>
+  <div class="ProductCmp">
+      <img src="http://via.placeholder.com/350x350">
+      <div>
+          <h2>{{ product.title}}</h2>
+          <span>Location: {{ product.locationNum}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCmp',
+  props: {
+      product: Object
+  },
+  
+}
+</script>
--- src/view/List.vue
+++ src/view/List.vue
@@ -3,6 +3,14 @@
     height: calc(100vh - 5em);
     overflow: auto;
 }
+ul{
+    padding: 0;
+    margin: 0;
+}
+.ProductCmp{
+    width: 50%;
+    float: left;
+}
 </style>
 <template>
   <section>
@@ -10,6 +18,9 @@
                  :hasRightBtn="headerData.hasRight"  :rightBtnText="headerData.rightBtnText"
                  @onRightBtnClick="add()"></HeaderNav>
       <section class="main">
+          <ul>
+              <ProductCmp v-for="product in products" :key="product.id" :product="product"></ProductCmp>
+          </ul>
       </section>
       <BottomNav :parent="viewName"></BottomNav>
   </section>
@@ -18,11 +29,14 @@
 import BottomNav from '@/components/BottomNav.vue'
 import HeaderNav from '@/components/HeaderNav.vue'
 import utilities from '@/utilities/utilities'
+import ProductCmp from '@/components/ProductCmp.vue'
+
 export default {
   name:'List',
   components:{
       'BottomNav': BottomNav,
-      'HeaderNav': HeaderNav
+      'HeaderNav': HeaderNav,
+      'ProductCmp': ProductCmp
   },
   data(){
       return {
@@ -31,7 +45,14 @@ export default {
               titleAlign: HeaderNav.CONFIG.CENTER,
               hasRight: true,
               rightBtnText: 'Add'
-          }
+          },
+          products:[
+              { id: 0, title: "A", locationNum: 45},
+              { id: 1, title: "B", locationNum: 25},
+              { id: 2, title: "C", locationNum: 45},
+              { id: 3, title: "D", locationNum: 445},
+              { id: 4, title: "E", locationNum: 495},
+          ],
       }
   },
   computed:{
